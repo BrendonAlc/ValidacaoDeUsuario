0af373a0004a0ed3597b8152b616fb109b9d628a
Brendon Rodrigo Alcântara <92334562+BrendonAlc@users.noreply.github.com>
2023-02-20 23:24:08 -0300

    Refatoração de código para teste de usuario válido e inválido
    Refatoração de código para teste de usuário válido e inválido.

--- cypress/e2e/AluraPic/RegistroDeUsuario.spec.cy.js
+++ cypress/e2e/AluraPic/RegistroDeUsuario.spec.cy.js
@@ -18,7 +18,7 @@ describe('Login e registro de usuarios alura pic', () => {
     it('verifica mensagens de email invalido', () => {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
-        cy.get('input[formcontrolname="email"]').type('jacqueline');
+        cy.get('input[formcontrolname="email"]').type('brendon');
         cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
      
     })
@@ -26,10 +26,26 @@ describe('Login e registro de usuarios alura pic', () => {
     it('verifica mensagens de senha com menos de 8 caracteres', () => {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
-        cy.get('input[formcontrolname="password"]').type('123');
+        cy.get('input[formcontrolname="password"]').type('321');
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
      
     })
+
+    //verificação de usuario válido
+    it.only('fazer login de usuario válido', () => {
+        cy.login('luciana', '12345678')
+        cy.contains('a', '(Logout)').should('be.visible');
+     
+    })
+
+    //verificação de usuario invalido
+    it.only('fazer login de usuario invalido', () => {
+        cy.login('brendon', '1234');
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password')
+        })
+    })
+
 
 })
